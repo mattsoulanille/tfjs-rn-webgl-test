ba15d471d6a7f39b737921b23382c517bbea5805
Matthew Soulanille <msoulanille@google.com>
2021-01-22 08:51:34 -0800

    Test loading a model.json file

--- App.tsx
+++ App.tsx
@@ -1,28 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import '@tensorflow/tfjs-react-native';
-import * as tf from '@tensorflow/tfjs-core';
+import * as tf from '@tensorflow/tfjs';
 import '@tensorflow/tfjs-backend-webgl';
-import '@tensorflow/tfjs-backend-cpu';
+import '@tensorflow/tfjs-react-native';
+import { bundleResourceIO } from '@tensorflow/tfjs-react-native';
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-async function loadBackend() {
+
+const modelDirName = 'model';
+const modelJson = require(`./assets/${modelDirName}/model.json`);
+const modelWeights = require(`./assets/${modelDirName}/group1-shard1of1.bin`);
+
+
+async function loadModel() {
+    tf.setBackend('rn-webgl');
     await tf.ready();
-    return tf.getBackend();
+
+    const model = await tf.loadGraphModel(
+	bundleResourceIO(modelJson, modelWeights));
+
+    // WARM-UP model
+    const res = tf.tidy(() =>
+	model.predict(tf.zeros([1, 10])),
+    ) as tf.Tensor; // <<---------- Error is thrown here
+
+    return model;
 }
 
 export default function App() {
-    const [result, setResult] = useState(['backend not loaded', false]);
+    const [model, setModel] = useState<tf.GraphModel | undefined | Error>();
 
-    if (!result[1]) {
-	loadBackend()
-	    .then((backend) => setResult([`Successfully loaded ${backend} backend`, true]))
-	    .catch((e) => setResult([`Failed to load backend: ${e}`, true]));
-    }
-    
+    useEffect(() => {
+	if (! model) {
+	    loadModel().then(model => {
+		setModel(model);
+	    })
+	}
+    }, []);
+
     return (
 	<View style={styles.container}>
-            <Text>{result[0]}</Text>
+            <Text>{
+		model instanceof tf.GraphModel
+		? model.predict(tf.randomNormal([1,10])).toString()
+		: 'loading'
+	    }</Text>
             <StatusBar style="auto" />
 	</View>
     );
